3d304fed667e1e37660b1be2db0691f738a8be93
Tirth-Thakkar <tirth1226@gmail.com>
2024-02-03 21:44:01 -0800

    Some slight Phaser Changes

--- _site/src/start.js
+++ _site/src/start.js
@@ -27,8 +27,8 @@ var gameConfig = {
 	scale: {
 		mode: Phaser.Scale.FIT,
 		autoCenter: Phaser.Scale.CENTER_BOTH,
-		width: 640,
-		height: 960
+		width: window.innerWidth,
+		height: window.innerHeight,
 	},
 	scene: [Boot, Preloader, MainMenu, Settings, Story, Game]
 }
