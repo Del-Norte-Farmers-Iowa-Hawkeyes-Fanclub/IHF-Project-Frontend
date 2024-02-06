d2ae897019a61061f7704c6e8ee8ac3bb46611b3
h4seeb-cmd <mirzahbeg123@gmail.com>
2024-02-06 09:10:47 -0800

    fixing image aspect ratio

--- _site/src/MainMenu.js
+++ _site/src/MainMenu.js
@@ -4,7 +4,7 @@ class MainMenu extends Phaser.Scene {
         this.bgFilesLoaded = false;
     }
     create() {
-        this.add.sprite(-800, -600, 'background').setOrigin(0,0);
+        this.add.sprite(0, -80, 'background').setOrigin(0,0);
     
         this.add.sprite(0, 0, `titleAuthor`).setOrigin(0,0);
 		EPT.Storage.initUnset('EPT-highscore', 0);
