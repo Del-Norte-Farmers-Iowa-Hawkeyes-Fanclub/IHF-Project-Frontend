46860057a89b5d8fbd90c077feb1c4b8a0b65008
Tirth-Thakkar <tirth1226@gmail.com>
2024-02-06 11:13:57 -0800

    some additions

---
+++ src/Leaderboard.js
@@ -0,0 +1,47 @@
+Phaser.GameObjects.GameObjectFactory.register('Leaderboard', function () {
+    var scene = new Leaderboard();
+    this.scene.add(scene.scene.key, scene);
+    return scene;
+});
+
+class Leaderboard extends Phaser.Scene {
+    constructor() {
+        super('Leaderboard');
+    }
+
+    preload ()
+    {
+        
+    }
+
+    create ()
+    {
+        
+        this.add.sprite(0, 0, 'background').setOrigin(0, 0);
+        var fontStory = { font: '30px ' + EPT.text['FONT'], fill: '#ffde00', stroke: '#000', strokeThickness: 5, align: 'center' };
+        // var textStory = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY-(0.75*this.cameras.main.centerY), EPT.text['login-title'], fontStory);
+        textStory.setOrigin(0.5);
+        this.tweens.add({
+            targets: element,
+            y: 300,
+            duration: 3000,
+            ease: 'Power3'
+        });
+
+        var buttonContinue = new Button(this.cameras.main.width - 20, this.cameras.main.height - 20, 'button-continue', this.clickContinue, this);
+        buttonContinue.setOrigin(1);
+
+        buttonContinue.x = this.cameras.main.width + buttonContinue.width + 20;
+        this.tweens.add({ targets: buttonContinue, x: this.cameras.main.width - 20, duration: 500, ease: 'Back' });
+
+        this.keyEnter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+        this.keyEnter.on('down', function (key, event) { this.clickContinue(); }, this);
+
+        this.cameras.main.fadeIn(250, 0, 0, 0);
+
+    }
+    clickContinue() {
+        EPT.Sfx.play('click');
+        EPT.fadeOutScene('Story', this);
+    }
+}
