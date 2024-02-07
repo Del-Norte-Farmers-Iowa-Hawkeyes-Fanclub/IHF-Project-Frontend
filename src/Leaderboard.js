6e748904ca2cd222baf6ad81d01ca706fadf0b84
Tirth-Thakkar <tirth1226@gmail.com>
2024-02-06 17:23:58 -0800

    better functionality

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,50 +1,71 @@
-let game, scores;
+// Define 'scores' globally to make it accessible throughout the code
+let scores;
+
+// Register Leaderboard as a GameObjectFactory
 Phaser.GameObjects.GameObjectFactory.register('Leaderboard', function () {
     var scene = new Leaderboard();
     this.scene.add(scene.scene.key, scene);
     return scene;
 });
 
+// Define Leaderboard class
 class Leaderboard extends Phaser.Scene {
     constructor() {
         super('Leaderboard');
         this.scores = [];
     }
+
     preload() {
+        // Load bitmap font
         this.load.bitmapFont('arcade', '/img/arcade.png', '/arcade.xml');
     }
+
     create() {
+        // Add background sprite
         this.add.sprite(0, 0, 'background').setOrigin(0, 0);
+
+        // Add leaderboard text
         var fontStory = { font: '30px ' + EPT.text['FONT'], fill: '#ffde00', stroke: '#000', strokeThickness: 5, align: 'center' };
         var textStory = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY - (0.75 * this.cameras.main.centerY), EPT.text['leaderboard-text'], fontStory);
         textStory.setOrigin(0.5);
 
+        // Add continue button
         var buttonContinue = new Button(this.cameras.main.width - 20, this.cameras.main.height - 20, 'button-continue', this.clickContinue, this);
         buttonContinue.setOrigin(1);
-
         buttonContinue.x = this.cameras.main.width + buttonContinue.width + 20;
         this.tweens.add({ targets: buttonContinue, x: this.cameras.main.width - 20, duration: 500, ease: 'Back' });
 
+        // Handle Enter key press
         this.keyEnter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
         this.keyEnter.on('down', function (key, event) { this.clickContinue(); }, this);
 
+        // Fade in camera
         this.cameras.main.fadeIn(250, 0, 0, 0);
         
-        this.add.bitmapText(100, 110, 'arcade', 'Eco Score | Profit Primary | Crop | Username').setTint(0xffffff);
-        for (let i = 1; i < 6; i++) {
-            if (scores[i-1]) {
-                this.add.bitmapText(100, 160 + 50 * i, 'arcade', ` ${scores[i-1].eco} | ${scores[i-1].profit} | ${scores[i-1].primaryCrop}| ${scores[i-1].username}`).setTint(0xffffff).setLetterSpacing(-5);
+        // Add leaderboard text
+        this.add.bitmapText(100, 110, 'arcade', 'Eco Score | Profit | Primary Crop | Username').setTint(0xffffff);
+
+        // Display scores
+        for (let i = 0; i < 5; i++) {
+            const scoreData = scores[i];
+            if (scoreData) {
+                const { eco, cash, primaryCrop, name } = scoreData;
+                const text = `${eco}     ${cash}     ${primaryCrop}     ${name}`;
+                this.add.bitmapText(100, 160 + 50 * (i + 1), 'arcade', text).setTint(0xffffff).setLetterSpacing(-5);
             } else {
-                this.add.bitmapText(100, 160 + 50 * i, 'arcade', ` ---      ---    ---    ---`).setTint(0xffffff).setLetterSpacing(-5);
+                this.add.bitmapText(100, 160 + 50 * (i + 1), 'arcade', ` ---      ---     ---      ---`).setTint(0xffffff).setLetterSpacing(-5);
             }
         }
     }
+
     clickContinue() {
+        // Play click sound and fade out scene
         EPT.Sfx.play('click');
         EPT.fadeOutScene('Story', this);
     }
 }
 
+// Perform AJAX request to get scores data
 $.ajax({
     type: 'GET',
     url: 'http://localhost:6942/api/person/',
@@ -53,9 +74,10 @@ $.ajax({
     },
     success: function(data) {
         scores = data;
+        // Once data is retrieved, create leaderboard scene
+        // game.scene.add('Leaderboard', Leaderboard, true);
     },
     error: function(xhr) {
         console.log(xhr);
     }
 });
-
